23d455eea7f9aed550b9c0fe256d12fd1bbeaa5f
Claymor <m.krestnicov@gmail.com>
2015-07-04 17:15:36 -0700

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 for dev ss 111</div>
+                <div class="title">Laravel 5 Back to normal!</div>
             </div>
         </div>
     </body>
